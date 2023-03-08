47837c97fe8da4f78accf5a453264dc6ae7b472d
Guilherme S.Fernandes <guilherme.fernandes.sou@gmail.com>
2023-03-07 21:14:52 -0300

    Blog Pessoal Em Andamento

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,56 +1,54 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography} from '@material-ui/core';
 import {Box} from '@mui/material';
-
-
+import { Link } from 'react-router-dom';
+import './Navbar.css'
 function Navbar() {
-
-   const estilo = {
-        background: 'black',
-           }
-
     return (
         <>
-            <AppBar className='cursor' position="static" style={estilo} >
-                <Toolbar variant="regular">
-                    <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h6" color="inherit">
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Home
+                                home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Postagens
+                                postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Temas
+                                temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Cadastrar Temas
+                                cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                Logout
-                            </Typography>
-                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    logout
+                                </Typography>
+                            </Box>
+                        </Link>
+                        
                     </Box>
 
                 </Toolbar>
             </AppBar>
         </>
-    );
+    )
 }
 
 export default Navbar;
